e404f6795b61b4fb71b37ad530d077753ace8e98
VictorMoraisBH <victormoraisbh@hotmail.com>
2024-08-07 20:51:42 -0300

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,26 @@
-"use strict";
-
 // Array de objetos contendo nome e nota dos alunos
-var alunos = [
-    { nome: "João", nota: 7 },
-    { nome: "Maria", nota: 5 },
-    { nome: "Pedro", nota: 6 },
-    { nome: "Ana", nota: 8 },
-    { nome: "Carla", nota: 4 }
-];
+const alunos = [{
+  nome: "João",
+  nota: 7
+}, {
+  nome: "Maria",
+  nota: 5
+}, {
+  nome: "Pedro",
+  nota: 6
+}, {
+  nome: "Ana",
+  nota: 8
+}, {
+  nome: "Carla",
+  nota: 4
+}];
 
 // Função que retorna apenas os alunos com nota maior ou igual a 6
-var aprovados = function aprovados(alunos) {
-    return alunos.filter(function (aluno) {
-        return aluno.nota >= 6;
-    });
+const aprovados = alunos => {
+  return alunos.filter(aluno => aluno.nota >= 6);
 };
 
 // Testando a função
-var alunosAprovados = aprovados(alunos);
+const alunosAprovados = aprovados(alunos);
 console.log(alunosAprovados);
